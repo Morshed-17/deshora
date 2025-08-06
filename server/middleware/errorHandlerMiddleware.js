d8c3d904bc6e08486ae4a5f4a7cf29f3608455f9
Morshed <morshedmto@gamil.com>
2025-08-06 20:53:12 +0600

    first commit

---
+++ server/middleware/errorHandlerMiddleware.js
@@ -0,0 +1,12 @@
+const errorHandlerMiddleware = async (err, req, res, next) => {
+  if (!res.statusCode || res.statusCode === 200) {
+    res.status(500);
+  }
+  res.json({
+    error: "Internal server Error",
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+};
+
+export default errorHandlerMiddleware;
